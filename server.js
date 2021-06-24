b5bbb6d46d5ba9d03d06dd6a7cb349e51d5ee0e0
asm@dostavista.ru <wolfstonik>
2021-06-24 22:27:03 +0300

    DV-0002: Add create endpoint

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 const express = require('express')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
 var app = express()
 var Data = require('./noteSchema')
 
-mongoose.connect('mongoDB://localhost/newDB')
+mongoose.connect('mongodb://localhost/newDB', { useNewUrlParser: true } )
+
 
 mongoose.connection.once("open", () => {
 
     console.log("Connected to DataBase")
 }).on("error", (error) => {
-console.log("Failed to connect" + error)
+console.log("Failed to connect " + error)
 })
 
+app.post("/create", (req, res) => {
+
+    var note  = Data ({
+        note: req.get("note"),
+        title: req.get("title"),
+        data: req.get("date")
+    })
+
+    note.save().then(() => {
+
+if (note.isNew == false) {
+    console.log("Saveed data")
+    res.send("Saved data.")
+} else {
+    console.log("Fail to save data.")
+}
+    })
+})
+
+ var server  = app.listen("8081", "192.168.1.5", () => {
+    console.log("Server is running.")  
+ })
