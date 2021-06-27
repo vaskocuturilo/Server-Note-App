737819ac405c54a40cda3edf2e63171ffe67f20c
asm@dostavista.ru <wolfstonik>
2021-06-27 17:00:55 +0300

    Dv-0007: Add update service.

--- server.js
+++ server.js
@@ -9,29 +9,63 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.status(200).json("Server is work fine.")
+    try {
+        res.status(200).json("Server is work fine.")
+    } catch (e) {
+        res.status(500).json(e)
+    }
 })
 
 app.get('/fetch', async (req, res) => {
-    const post = await Post.find({})
-    res.status(200).json(post)
+    try {
+        const post = await Post.find({})
+        res.status(200).json(post)
+    } catch (e) {
+        res.status(500).json(e)
+    }
+
 })
 
 app.post("/create", async (req, res) => {
-    const {title, date, note} = req.body
-    const post = await Post.create({title, date, note})
-    res.status(200).json(post)
+    try {
+        const {title, date, note} = req.body
+        const post = await Post.create({title, date, note})
+        res.status(200).json(post)
+    } catch (e) {
+        res.status(500).json(e)
+    }
 })
 
 app.post("/remove", async (req, res) => {
-    const {id} = req.body
-    const post = await Post.findOneAndRemove({id: req.get("id")})
-    res.status(200).json('Item was delete.')
+    try {
+        const id = req.body
+        const post = await Post.remove({id: req.get("id")})
+        res.status(200).json('Item was delete.')
+    } catch (e) {
+        res.status(500).json(e)
+    }
+})
+
+app.post("/update", async (req, res) => {
+    try {
+        const id = req.body
+        const {title, date, note} = req.body
+        const post = await Post.findOneAndUpdate({
+                id: req.get("id")
+            },
+            {
+                title: req.get("title"),
+                date: req.get("date"),
+                note: req.get("note"),
+            }, {new: true})
+    } catch (e) {
+        res.status(500).json(e)
+    }
 })
 
 async function startApplication() {
     try {
-        await mongoose.connect(DB_URL, {useNewUrlParser: true}, {useUnifiedTopology: true}, {useFindAndModify: false })
+        await mongoose.connect(DB_URL, {useNewUrlParser: true}, {useUnifiedTopology: true}, {useFindAndModify: false})
         app.listen(PORT, () => console.log("SERVER STARTED ON PORT " + PORT))
 
     } catch (e) {
